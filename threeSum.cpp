f7fefd27d90f3e203a41069ef44d0b8390ea387e
4O4-N0T-Found <648759521@qq.com>
2020-06-17 22:50:17 +0800

    找三数，使和等于0。leetcode15题

--- threeSum.cpp
+++ threeSum.cpp
@@ -4,39 +4,24 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int>& nums) {
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-    vector<vector<int>> ans;
-    // 枚举 a
-    for (int first = 0; first < n; ++first) {
-        // 需要和上一次枚举的数不相同
-        if (first > 0 && nums[first] == nums[first - 1]) {
+    vector<vector<int>> res;
+    sort(nums.begin(),nums.end());
+    int first, second, third, count=nums.size();
+    for(first=0; first<count; first++) {
+        if(first>0 && nums[first]==nums[first-1])
             continue;
-        }
-        // c 对应的指针初始指向数组的最右端
-        int third = n - 1;
-        int target = -nums[first];
-        // 枚举 b
-        for (int second = first + 1; second < n; ++second) {
-            // 需要和上一次枚举的数不相同
-            if (second > first + 1 && nums[second] == nums[second - 1]) {
+        third = count-1;
+        for(second=first+1; second<count; second++) {
+            if(second>first+1 && nums[second]==nums[second-1])
                 continue;
-            }
-            // 需要保证 b 的指针在 c 的指针的左侧
-            while (second < third && nums[second] + nums[third] > target) {
-                --third;
-            }
-            // 如果指针重合，随着 b 后续的增加
-            // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
-            if (second == third) {
-                break;
-            }
-            if (nums[second] + nums[third] == target) {
-                ans.push_back({nums[first], nums[second], nums[third]});
-            }
+            while(second < third && nums[first]+nums[second]+nums[third] > 0)
+                third--;
+            if(second == third) break;
+            if(nums[first]+nums[second]+nums[third] == 0)
+                res.push_back({nums[first], nums[second], nums[third]});
         }
     }
-    return ans;
+    return res;
 }
 
 int main() {
